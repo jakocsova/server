f06fff247254404526751baf94dc82add48972f8
unknown <vercsij@gmail.com>
2018-10-25 11:44:25 +0200

    xml

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var express = require("express");
 var server = express();
 var path = require("path");
 
-var PORT=3000
+var PORT=3021
 
-server.get('/getCapabilities', function (request, response) {
-    
-response.sendFile(path.join(__dirname + "\\CapabilitiesDokument.xml"))
+server.get('/wms', function (request, response) {
+    var params=request.query;
+    console.log(params);
+    if(params.SERVICE==="WMS" && params.REQUEST==="GetCapabilities"){
+      response.sendFile(path.join(__dirname , "nase_vrstvy.xml"))
+}else if (params.service==="wms"&& params.request==="GetMap"){
+console.log("idem robit get map")
+}else{
+  response.send("nepodporovana metoda")
+}
+
 })
-
-server.get("/some/path", function(request, response) {
-    console.log(request.query.jahoda);
-    response.send(request.query);
-  });
-
 
 server.listen(PORT, function() {
     console.log("Server listening on port " + PORT + "!");
