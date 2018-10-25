03ad1587b56d8a40c94b5a2db3a4a9f58c12f892
unknown <vercsij@gmail.com>
2018-10-25 09:18:57 +0200

    xml

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-var x=16;
-var k=[5,6,3,4,"hhhhh"];
-console.log(k + "hello");
+var express = require("express");
+var server = express();
+var path = require("path");
+
+var PORT=3000
+
+server.get('/getCapabilities', function (request, response) {
+    
+response.sendFile(path.join(__dirname + "\\CapabilitiesDokument.xml"))
+})
+
+server.get("/some/path", function(request, response) {
+    console.log(request.query.jahoda);
+    response.send(request.query);
+  });
+
+
+server.listen(PORT, function() {
+    console.log("Server listening on port " + PORT + "!");
+  });
+
+
